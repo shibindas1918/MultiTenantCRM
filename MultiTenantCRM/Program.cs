7509d41906daa24fc37a260554fde504ffee2a93
sibin <sibindas1806@gmail.com>
2024-12-23 14:22:29 +0530

    Connection string

--- MultiTenantCRM/Program.cs
+++ MultiTenantCRM/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Added Connection string 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
