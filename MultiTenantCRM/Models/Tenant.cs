199ee942286f69c412d029491536b824d0dac941
sibin <sibindas1806@gmail.com>
2024-12-26 12:38:57 +0530

    //

--- MultiTenantCRM/Models/Tenant.cs
+++ MultiTenantCRM/Models/Tenant.cs
@@ -7,7 +7,7 @@
         public string Domain { get; set; }
         public DateTime CreatedDate { get; set; }
         public ICollection<User> Users { get; set; }
-        public ICollection<Contact> Customers { get; set; }
+        public ICollection<Contact> Customers { get; set; } = new List<Contact>();
 
     }
 }
